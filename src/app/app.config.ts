de101e0ef517146089b2e23c9ff6080d86eab332
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-03-27 20:17:19 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-172c7","appId":"1:1098632374519:web:2f7a316a716c775b1f4bf2","storageBucket":"dabubble-172c7.appspot.com","apiKey":"AIzaSyBcMF4K_odDgT1p9mYESQ--a5Et3BJgLzk","authDomain":"dabubble-172c7.firebaseapp.com","messagingSenderId":"1098632374519"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
