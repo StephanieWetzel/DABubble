c1b30e8df6725a8088508d394d37bb3bb2b1119e
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-03-28 15:20:32 +0100

    added login component + font family

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component, ViewChild, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 // import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
-    RouterLink, 
-    MatIconModule, 
-    MatButtonModule, 
+  imports: [RouterOutlet,
+    RouterLink,
+    MatIconModule,
+    MatButtonModule,
     MatToolbarModule,
     MatSidenavModule,
     MatExpansionModule,
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
+    LoginComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,5 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-172c7","appId":"1:1098632374519:web:2f7a316a716c775b1f4bf2","storageBucket":"dabubble-172c7.appspot.com","apiKey":"AIzaSyBcMF4K_odDgT1p9mYESQ--a5Et3BJgLzk","authDomain":"dabubble-172c7.firebaseapp.com","messagingSenderId":"1098632374519"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-172c7","appId":"1:1098632374519:web:2f7a316a716c775b1f4bf2","storageBucket":"dabubble-172c7.appspot.com","apiKey":"AIzaSyBcMF4K_odDgT1p9mYESQ--a5Et3BJgLzk","authDomain":"dabubble-172c7.firebaseapp.com","messagingSenderId":"1098632374519"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync(), provideAnimationsAsync()]
 };
