26deb743f7616b4db9098d5db6078d6594dc6605
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-03-28 17:19:06 +0100

    worked on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 }
