ba2c350742de2fed7395ee663ea45d939589a6d4
Henrik Sorg <henrik@reif-sorg.de>
2024-03-28 17:33:04 +0100

    main content component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,7 @@
 import { Component, ViewChild, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
-import { HeaderComponent } from './header/header.component';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+
 import { LoginComponent } from './login/login.component';
 
 @Component({
@@ -17,12 +9,6 @@ import { LoginComponent } from './login/login.component';
   standalone: true,
   imports: [RouterOutlet,
     RouterLink,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatExpansionModule,
-    HeaderComponent,
     LoginComponent
   ],
   templateUrl: './app.component.html',
@@ -30,5 +16,5 @@ import { LoginComponent } from './login/login.component';
 })
 export class AppComponent {
   title = 'DA-Bubble';
-  @ViewChild(MatAccordion) accordion!: MatAccordion;
+  
 }
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
+
+@Component({
+    selector: 'app-main-content',
+    standalone: true,
+    templateUrl: './main-content.component.html',
+    styleUrl: './main-content.component.scss',
+    imports: [MatIconModule, 
+        MatButtonModule, 
+        MatToolbarModule, 
+        MatSidenavModule, 
+        MatAccordion, 
+        MatExpansionModule, 
+        HeaderComponent, 
+        RouterOutlet,
+        RouterLink
+      ]
+})
+export class MainContentComponent {
+
+}
