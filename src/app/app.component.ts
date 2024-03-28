a381e7221064f70ee92fcdba8c2c0c45b6a13a3a
Henrik Sorg <henrik@reif-sorg.de>
2024-03-28 12:41:33 +0100

    basic structure main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+// import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, 
+    RouterLink, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatToolbarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'da-bubble';
+  title = 'DA-Bubble';
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
 }
