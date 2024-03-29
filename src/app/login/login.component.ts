4f550dc6258b04ab93bd167021adef33fead7853
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-03-29 02:54:26 +0100

    almost finished login component design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatFormFieldModule],
+  imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
