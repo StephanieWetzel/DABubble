ea143e7470baeb402b9d1bc099143e89b398048f
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-03-29 21:59:12 +0100

    fixed styling in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,27 @@
+import { CommonModule, NgIf, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, Validators, FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatInputModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatButtonModule, NgIf, NgClass, NgStyle],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passwordFormControl = new FormControl('', [Validators.required]);
+  formData: FormGroup = this.fbuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+
+
+  constructor(private fbuilder: FormBuilder) {
+
+  }
 }
