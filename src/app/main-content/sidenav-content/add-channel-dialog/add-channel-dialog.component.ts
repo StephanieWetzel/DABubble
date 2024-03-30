76f012d798840219ff3c5835a9cc9ed4ae1d78b0
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-03-30 12:00:52 +0100

    implemented dialog for adding channels -> not finished yet

--- src/app/main-content/sidenav-content/add-channel-dialog/add-channel-dialog.component.ts
+++ src/app/main-content/sidenav-content/add-channel-dialog/add-channel-dialog.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-channel-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatInputModule],
   templateUrl: './add-channel-dialog.component.html',
   styleUrl: './add-channel-dialog.component.scss'
 })
--- src/app/main-content/sidenav-content/sidenav-content.component.ts
+++ src/app/main-content/sidenav-content/sidenav-content.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { User } from './user-test.class';
 import { CommonModule } from '@angular/common';
+import { AddChannelDialogComponent } from "./add-channel-dialog/add-channel-dialog.component";
 
 const EDIT_SQUARE_ICON = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" 
   width="24"><path d="M232-172q-26 0-43-17t-17-43v-496q0-26 17-43t43-17h329l-28 28H232q-12 0-22 10t-10 22v496q0 12 10 22t22 10h496q12 0 22-10t10-22v-306l28-28v334q0 26-17 43t-43
@@ -11,12 +12,12 @@ const EDIT_SQUARE_ICON = `<svg xmlns="http://www.w3.org/2000/svg" height="24" vi
   41 44ZM440-440h40l277-277-20-20-23-22-274 274v45Zm297-297-23-22 23 22 20 20-20-20Z"/></svg>`;
 
 @Component({
-  selector: 'app-sidenav-content',
-  standalone: true,
-  imports: [CommonModule, MatExpansionModule, MatIconModule],
-  templateUrl: './sidenav-content.component.html',
-  styleUrl: './sidenav-content.component.scss',
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-sidenav-content',
+    standalone: true,
+    templateUrl: './sidenav-content.component.html',
+    styleUrl: './sidenav-content.component.scss',
+    encapsulation: ViewEncapsulation.None,
+    imports: [CommonModule, MatExpansionModule, MatIconModule, AddChannelDialogComponent, MatDialogModule]
 })
 export class SidenavContentComponent {
   isUserOnline: boolean = true;
@@ -24,5 +25,13 @@ export class SidenavContentComponent {
   new User(false, 'Henrik', '', './assets/img/avatar_clean1.png'),
   new User(false, 'Sebastian', '', './assets/img/avatar_clean2.png')
 ]
+  constructor(public dialog: MatDialog) {}
+
+  addChannel() {
+    const dialogRef = this.dialog.open(AddChannelDialogComponent, {
+      panelClass: 'custom-add-channel-dialog'
+    });
+  }
+
 }
 
