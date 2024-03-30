37be28007df0c5faf7996bbd5f16bb5adff2d9a6
Henrik Sorg <henrik@reif-sorg.de>
2024-03-30 17:32:24 +0100

    style of single-message

--- src/app/main-content/chat/messages/messages.component.ts
+++ src/app/main-content/chat/messages/messages.component.ts
@@ -1,8 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { ChatService } from '../chat-service/chat.service';
 import { Message } from '../../../../assets/models/message.class';
 import { NgFor } from '@angular/common';
-import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-messages',
@@ -13,37 +12,18 @@ import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 })
 
 export class MessagesComponent {
-  private firestore: Firestore = inject(Firestore);
 
   messages;
-  // unsubMessage;
+  time: any;
+  date: any;
 
   constructor(private chatService: ChatService) {
     this.messages = this.chatService.messages;
-    // this.unsubMessage = this.getMessages();
   }
+
 
   getList(): Message[] {
     this.messages = this.chatService.messages;
-    console.log(this.chatService.messages);
     return this.chatService.messages;
   }
-
-  // getMessages() {
-  //   onSnapshot(this.getMessagesRef(), (collection) => {
-  //     this.messages = [];
-  //     collection.forEach(doc => {
-  //       let data = doc.data();
-  //       let message = new Message({ data, id: doc.id})
-  //       this.messages.push(message);
-  //       console.log(this.messages);
-  //     });
-  //   })
-
-  // }
-
-
-  // getMessagesRef() {
-  //   return collection(this.firestore, 'messages');
-  // }
 }
