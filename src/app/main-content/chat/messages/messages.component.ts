ed0448d8826fbd4fdcc13b8a65f9453bc80cd32f
Henrik Sorg <henrik@reif-sorg.de>
2024-04-01 21:46:02 +0200

    hover for message options

--- src/app/main-content/chat/messages/messages.component.ts
+++ src/app/main-content/chat/messages/messages.component.ts
@@ -4,11 +4,12 @@ import { Message } from '../../../../assets/models/message.class';
 import { NgFor } from '@angular/common';
 import { CustomDatePipe } from './date-pipe/custom-date.pipe';
 import { CustomTimePipe } from './time-pipe/custom-time.pipe';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [NgFor, CustomDatePipe, CustomTimePipe],
+  imports: [NgFor, CustomDatePipe, CustomTimePipe, MatIconModule, MatIcon],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
