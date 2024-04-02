ef918b3a3837ba27f23a9e0e18b59f3dc3b71d2f
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-04-02 18:05:34 +0200

    implemented firestore in authentication service

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,62 +1,66 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { User } from "../models/user.class";
+import { User } from "../assets/models/user.class";
+import { Firestore } from "@angular/fire/firestore/firebase";
+import { inject } from "@angular/core";
+
+export class UserDetailComponent {
+    firestore: Firestore = inject(Firestore);
 
 
-// config of our project
-const firebaseConfig = {
-    "projectId": "dabubble-172c7",
-    "appId": "1:1098632374519:web:2f7a316a716c775b1f4bf2",
-    "storageBucket": "dabubble-172c7.appspot.com",
-    "apiKey": "AIzaSyBcMF4K_odDgT1p9mYESQ--a5Et3BJgLzk",
-    "authDomain": "dabubble-172c7.firebaseapp.com",
-    "messagingSenderId": "1098632374519"
-};
+    // config of our project
+    firebaseConfig = {
+        "projectId": "dabubble-172c7",
+        "appId": "1:1098632374519:web:2f7a316a716c775b1f4bf2",
+        "storageBucket": "dabubble-172c7.appspot.com",
+        "apiKey": "AIzaSyBcMF4K_odDgT1p9mYESQ--a5Et3BJgLzk",
+        "authDomain": "dabubble-172c7.firebaseapp.com",
+        "messagingSenderId": "1098632374519"
+    };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+    // Initialize Firebase
+    // const app = initializeApp(this.firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-
-// register
-createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-    });
+    // // Initialize Firebase Authentication and get a reference to the service
+    // const auth = getAuth(this.app);
 
 
-// login
-signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    });
+    //     createUserWithEmailAndPassword: any(auth, email, password)
+    //     .then((userCredential) => {
+    //     // Signed up 
+    //     const user = userCredential.user;
+    //     // ...
+    //     })
+    //     .catch ((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //     // ..
+    // });
 
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        // ...
-    } else {
-        // User is signed out
-        // ...
-    }
-});
+    // // login
+    // signInWithEmailAndPassword(auth, email, password)
+    //     .then((userCredential) => {
+    //         // Signed in 
+    //         const user = userCredential.user;
+    //         // ...
+    //     })
+    //     .catch((error) => {
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+    //     });
 
 
+    // onAuthStateChanged(auth, (user) => {
+    //     if (user) {
+    //         // User is signed in, see docs for a list of available properties
+    //         // https://firebase.google.com/docs/reference/js/auth.user
+    //         const uid = user.uid;
+    //         // ...
+    //     } else {
+    //         // User is signed out
+    //         // ...
+    //     }
+    // });
+
+}
