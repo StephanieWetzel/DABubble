dbcd2dda1de3b085a5aaa4167a84e8213fcddc92
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-04-09 20:25:31 +0200

    added authentication for header -> u can now loginand the header will display the current user

--- src/app/registration/authentication.service.ts
+++ src/app/registration/authentication.service.ts
@@ -1,46 +1,54 @@
 // import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-import { Firestore } from "@angular/fire/firestore/firebase";
-import { inject, Injectable } from "@angular/core";
-import { Auth, signInAnonymously, signOut } from "@angular/fire/auth";
-import { User } from "../../assets/models/user.class";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { Firestore } from '@angular/fire/firestore/firebase';
+import { inject, Injectable } from '@angular/core';
+import { Auth, signInAnonymously, signOut } from '@angular/fire/auth';
+import { User } from '../../assets/models/user.class';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class AuthenticationService {
-    currentUser: any;
+  currentUser: any;
 
-    constructor(public auth: Auth) { }
+  constructor(public auth: Auth) {}
 
+  signIn(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
 
-    signIn(email: string, password: string) {
-        return signInWithEmailAndPassword(this.auth, email, password);
-    }
+  signUp(email: string, password: string) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
 
+  signOut() {
+    return signOut(this.auth);
+  }
 
-    signUp(email: string, password: string) {
-        return createUserWithEmailAndPassword(this.auth, email, password);
-    }
+  signInAnonymously() {
+    const auth = getAuth();
+    return signInAnonymously(auth);
+  }
 
+  fetchCurrentUser() {
+    return this.currentUser;
+  }
 
-    signOut() {
-        return signOut(this.auth);
-    }
-
-
-    signInAnonymously() {
-        const auth = getAuth();
-        return signInAnonymously(auth);
-    }
-
-
-    fetchCurrentUser() {
-        return this.currentUser;
-    }
-
-
+  fetchLoggedUser() {
+    const auth = getAuth();
+    return new Promise((resolve, reject) => {
+      onAuthStateChanged(auth, (user) => {
+        if (user?.uid) {
+          resolve(user.uid); // gives the user id back if user is logged in
+        } else {
+          reject(new Error('No user logged in'));
+        }
+      });
+    });
+  }
 }
-
