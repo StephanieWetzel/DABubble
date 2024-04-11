b261c6fcdd61ba7d964d8a789dd156fe00696368
Stephanie Wetzel <140720168+StephanieWetzel@users.noreply.github.com>
2024-04-11 11:08:50 +0200

    clean coding

--- src/app/registration/register/choose-avatar/choose-avatar.component.ts
+++ src/app/registration/register/choose-avatar/choose-avatar.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { AuthenticationService } from '../../authentication.service';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Component({
@@ -24,21 +23,15 @@ export class ChooseAvatarComponent {
   isClicked: boolean = false;
   allAvatars = ['avatar_clean0.png', 'avatar_clean1.png', 'avatar_clean2.png', 'avatar_clean3.png', 'avatar_clean4.png', 'avatar_clean5.png'];
   clickedIndex: number = -1; // Index des zuletzt geklickten Elements
-
   selectedAvatarURL: string | any = '';
   userId: string | any = '';
 
   firestore: Firestore = inject(Firestore)
 
 
-  constructor(
-    private auth: AuthenticationService,
-    private router: Router,
-    private route: ActivatedRoute) {
-
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.route.params.subscribe((params) => {
       this.userId = params['id'];
-      console.log(this.userId)
     });
   }
 
@@ -64,15 +57,14 @@ export class ChooseAvatarComponent {
   }
 
 
-  async signUp(userId: string, selectedAvatarURL: string) {
+  async signUp(userId: string) {
     try {
       const userRef = doc(this.firestore, "user", userId);
       await setDoc(userRef, { avatar: this.selectedAvatarURL }, { merge: true });
+      this.router.navigate(['/main']);
       console.log('Avatar saved to Firebase:', this.selectedAvatarURL);
-      // Hier können Sie weitere Aktionen ausführen, z. B. eine Benachrichtigung anzeigen oder zur nächsten Ansicht navigieren
     } catch (error) {
       console.error('Error saving avatar to Firebase:', error);
-      // Hier können Sie Fehlerbehandlung durchführen, z. B. eine Fehlermeldung anzeigen
     }
   }
 
