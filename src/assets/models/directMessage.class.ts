72b45a59144745820c1591b38e4d2e036cc7e90f
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-04-14 13:34:27 +0200

    added DM- class to models

---
+++ src/assets/models/directMessage.class.ts
@@ -0,0 +1,19 @@
+export class DirectMessage {
+    member: any[];
+    id: string;
+    messages: any[]
+
+    constructor(obj?:any) {
+        this.member = obj ? obj.member: [];
+        this.id = obj ? obj.id: '';
+        this.messages = obj ? obj.messages: []
+    }
+
+    toJSON() {
+        return {
+            member: this.member,
+            id: this.id,
+            messages: this.messages
+        }
+    }
+}
